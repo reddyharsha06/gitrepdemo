3071ecbeb529db593f65d6a51f0fc59b8dc47678
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2019-05-19 02:55:27 +0530

    Cheat

--- src/main/scala/cg/samples/AirportDataReader.scala
+++ src/main/scala/cg/samples/AirportDataReader.scala
@@ -8,11 +8,26 @@ object AirportDataReader  {
   val logger = Logger.getLogger("micheal")
 
   def main(args: Array[String]): Unit = {
-    val fileName = "d:/inputData/airports.csv";
-    logger.info("Reading file " + fileName );
+//    logger.info("Reading file " + fileName );
+//    val lines = Source.fromFile(fileName).getLines();
+//    logger.info("File Read Successfully ");
+//    lines.foreach(line => logger.info(line));
+
+
+    val fileName = "d:/inputData/airports.csv"
     val lines = Source.fromFile(fileName).getLines();
-    logger.info("File Read Successfully ");
-    lines.foreach(line => logger.info(line));
+    lines foreach println
+    println(lines.size);
+
+//    val a = Array(1,2,3,4,5,6);
+//    a foreach println
+//    println(a.size);
+
+    //lines.filter
+    //lines.collect
+    //lines.map
+
+
 
     // the airport has lot of data
   }
